f741601f5e52a620729e74844aae0414e6995a2d
Ayesha PERERA <gperera@ltu.edu.au>
2022-10-14 10:16:00 +1100

    first push

---
+++ R/mul_by_three.R
@@ -0,0 +1,13 @@
+#' Multiply a number by three
+#'
+#' @param num A number that needs to be multiplied by 3
+#'
+#' @return A multiple
+#' @export
+#'
+#' @examples mul_by_three(3)
+mul_by_three <- function(num){
+
+  return(num * 3)
+
+}
---
+++ R/mul_by_two.R
@@ -0,0 +1,16 @@
+
+#' Multiply a number by 2
+#'
+#' @param num a numerical value that
+#'            needs to be multiplied by 2
+#'
+#' @return The value multiplied by 2
+#' @export
+#'
+#' @examples mul_by_two(2)
+#'
+mul_by_two <- function(num){
+
+  return(num * 2)
+
+}
